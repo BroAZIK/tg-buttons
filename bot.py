61be47db6affd11db4344f842bf7ba146e5f934f
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-09 18:25:09 +0500

    callback query data

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from handlers import (
     start,
+    callback_query_handler,
 )
 import os
 
@@ -13,7 +14,12 @@ def main():
     dispatcher = updater.dispatcher
 
     # add hendlers
-    dispatcher.add_handler(handler=CommandHandler(command='start', callback=start))
+    dispatcher.add_handler(
+        handler=CommandHandler(command='start', callback=start)
+    )
+    dispatcher.add_handler(
+        handler=CallbackQueryHandler(callback=callback_query_handler)
+    )
 
     # start polling
     updater.start_polling()
--- handlers.py
+++ handlers.py
@@ -5,9 +5,11 @@ from telegram.ext import CallbackContext
 def start(update: Update, context: CallbackContext):
     btn1 = InlineKeyboardButton(text='admin', url='https://t.me/jumanovdiyorbek')
     btn2 = InlineKeyboardButton(text='say hello', callback_data='say-hello')
+    btn3 = InlineKeyboardButton(text='say hi', callback_data='say-hi')
 
     inline_keyboard = [
-        [btn1, btn2]
+        [btn3, btn2],
+        [btn1]
     ]
     chat_id = update.message.chat.id
 
@@ -15,3 +17,12 @@ def start(update: Update, context: CallbackContext):
         chat_id, 'Salom, xush kelibsiz!', 
         reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
     )
+
+def callback_query_handler(update: Update, context: CallbackContext):
+    callback_query = update.callback_query
+
+    if callback_query.data == 'say-hello':
+        update.callback_query.message.reply_text(f'Hello')
+        
+    if callback_query.data == 'say-hi':
+        update.callback_query.message.reply_text(f'Hi')
